9b6ecea4f9e9325f7d456bd7ab58948f4f610b96
shahryar <s.shahryar75@gmail.com>
2016-07-23 04:00:21 +0430

    lots of things changed ! there is no way to explain them!

--- src/signUp.java
+++ src/signUp.java
@@ -1,12 +1,12 @@
 /**
  * Created by panizava on 19/07/2016.
  */
-import
 
 public class signUp {
-    USER user ;
+    USER User ;
     public signUp(USER user){
-        this.user=user;
+        this.User=user;
         // Now I have to store this user in the database! :
+
     }
 }
