dd47fdd5bd5dce8ce99fd3680d047d840e4703a9
shahryar <s.shahryar75@gmail.com>
2016-07-22 12:55:27 +0430

    src/userClass.java

--- src/userClass.java
+++ src/userClass.java
@@ -6,48 +6,52 @@ public class USER {
     private String lastName;
     private String password;
     private String phoneNumber;
+    public enum State{
+        online,
+        offline,
+        away
+    }
+    State state=State.online;
+    //constructors :
     public USER(String name,String lastName,String password,String phoneNumber){
         this.name=name;
         this.lastName=lastName;
         this.password=password;
         this.phoneNumber=phoneNumber;
+        this.state=State.online;
     }
     public USER(USER user){
-        name=getName();
-        lastName=getLastName();
-        password=getPassword();
-        phoneNumber=getPhoneNumber();
+        name=user.getName();
+        lastName=user.getLastName();
+        password=user.getPassword();
+        phoneNumber=user.getPhoneNumber();
+        state=user.getState();
     }
     // getters and setters :
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+    public State getState() { return state; }
+    public void setState(State state) {this.state = state;}
 }
