0433ecea877d5be7cdb005af8ab0c144f1edf9c2
shahryar <s.shahryar75@gmail.com>
2016-07-23 03:56:51 +0430

    this class is created , honestly! , to test other classes !(I know this is not the correct way!!))

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.sql.SQLException;
+
+/**
+ * Created by shahryar_slg on 22/07/2016.
+ */
+
+
+
+public class main {
+    public static void main(String args[]) throws SQLException {
+        DatabaseConection.connect();
+    }
+}
