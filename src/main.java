dd7aa707641dd8134403f1654a0e79d7b1377e6f
shahryar <s.shahryar75@gmail.com>
2016-07-23 06:02:44 +0430

    nothing important

--- src/main.java
+++ src/main.java
@@ -8,6 +8,6 @@ import java.sql.SQLException;
 
 public class main {
     public static void main(String args[]) throws SQLException {
-        DatabaseConection.connect();
+        DatabaseDriver.connect();
     }
 }
--- src/signUp.java
+++ src/signUp.java
@@ -1,7 +1,6 @@
 /**
  * Created by panizava on 19/07/2016.
  */
-
 public class signUp {
     USER User ;
     public signUp(USER user){
