8d3bf50c9cfbd59a019d11f8ff77656abddc881b
shahryar <s.shahryar75@gmail.com>
2016-07-29 13:38:20 +0430

    socketStuff class is created so that i can test the socket stuff

--- JCPClientSide/src/socketStuff.java
+++ JCPClientSide/src/socketStuff.java
@@ -1,8 +1,17 @@
+import java.io.IOException;
 import java.net.Socket;
 
 /**
  * Created by panizava on 27/07/2016.
  */
 public class socketStuff {
-    Socket s = new Socket()
+    public static void main(String args[]) throws IOException {
+        Socket cliSocket = new Socket("localhost",3571);
+        System.out.println(cliSocket.getPort());
+        System.out.println(cliSocket.getLocalPort());
+        System.out.println(cliSocket.getInetAddress());
+        /*while ( true ){
+        }*/
+    }
+
 }
