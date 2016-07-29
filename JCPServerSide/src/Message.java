c688fc35affb06e6724446b92f164b6fb5976079
shahryar <s.shahryar75@gmail.com>
2016-07-30 04:06:38 +0430

    message class created , this class is implemented so simple  , but things will get more complex later

--- JCPServerSide/src/Message.java
+++ JCPServerSide/src/Message.java
@@ -5,4 +5,34 @@ public class Message {
     private String sender;
     private String receiver ; // phonenumber will be saved in these two variables
     private String content ;
+
+    public Message(String sender, String receiver, String content) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+    }
+    //getter and setter :
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
 }
