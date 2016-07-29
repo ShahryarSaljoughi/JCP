70386157fbf8848aa4708a242a84c8b02c73ec51
shahryar <s.shahryar75@gmail.com>
2016-07-30 04:01:16 +0430

    just this fucking socketStuff!

--- JCPServerSide/src/socketStuff.java
+++ JCPServerSide/src/socketStuff.java
@@ -1,16 +1,23 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*
 
 /**
  * Created by panizava on 28/07/2016.
  */
 public class socketStuff {
+    private InputStream inStrm ;
+    private OutputStream outStrm ;
+    private ServerSocket ss;
+    private Socket acceptSocket;
     public static void main (String args[]) throws IOException {
         ServerSocket ss = new ServerSocket(3571);
         Socket acceptSocket= ss.accept();// this will be used to communicate with the client
         System.out.println("connection was made ... ");
-        System.out.println("conncted to this inetAddress : "+acceptSocket.getInetAddress()
+        System.out.println(
+                "connected to this inetAddress : "+acceptSocket.getInetAddress()
                                         +" ,"+acceptSocket.getPort());
+
     }
 }
