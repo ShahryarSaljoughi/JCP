4e2d177d0bae86214732e76ab4a44f933ffee334
shahryar <s.shahryar75@gmail.com>
2016-07-30 03:56:32 +0430

    Message class to have information of a sent text together!

---
+++ JCPServerSide/src/Message.java
@@ -0,0 +1,8 @@
+/**
+ * Created by panizava on 30/07/2016.
+ */
+public class Message {
+    private String sender;
+    private String receiver ; // phonenumber will be saved in these two variables
+    private String content ;
+}
