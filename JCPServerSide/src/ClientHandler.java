0352021a2fec00fd5a8eab9c67d288b8d6efeab5
shahryar <s.shahryar75@gmail.com>
2016-08-01 02:02:43 +0430

    nothing special !

--- JCPServerSide/src/ClientHandler.java
+++ JCPServerSide/src/ClientHandler.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  * Created by panizava on 30/07/2016.
@@ -10,10 +12,18 @@ public class ClientHandler extends Thread {
     private Socket clientSocket = null;
     private InputStream InStrm;
     private OutputStream OutStrm;
-    public ClientHandler(Socket clientSocket) throws IOException {
-        this.clientSocket = clientSocket;
+    private PrintWriter PWOut;
+    private java.util.Scanner ScnrIn;
+    public ClientHandler(ClientAccessor client) throws IOException {
+
+        /*his.clientSocket = client;
         InStrm = clientSocket.getInputStream();
         OutStrm = clientSocket.getOutputStream();
+        PWOut = new PrintWriter(OutStrm);
+        ScnrIn = new Scanner(InStrm);
+        */
     }
+
+
 
 }
